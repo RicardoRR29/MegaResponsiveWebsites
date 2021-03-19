488ddfb57a4320820e14022db4d037c3918dfcfc
RicardoRR29 <r3ifes@gmail.com>
2021-03-19 19:06:11 -0300

    foods done

--- Foods/script.js
+++ Foods/script.js
@@ -1,6 +1,23 @@
-const icons = document.querySelectorAll('.section-1-icons i')
+const menu = document.querySelector('.menu').addEventListener('click', () => {
+  document.querySelectorAll('.target').forEach((item) => {
+    item.classList.toggle('show')
+  })
+})
 
+const icons = document.querySelectorAll('.section-1-icons i')
+window.scroll(0,0)
+
+/*
+let j = 0
+
+setTimeout(() => {
+  setInterval(() => {
+  window.scroll(0, 50 * j)
+  j++
+}, 50)}, 5000)
+*/
 let i = 1
+
 
 setInterval(() => {
   i++
