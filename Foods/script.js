314cece6a1efec8a5c73462530c0a500d5fd9307
RicardoRR29 <r3ifes@gmail.com>
2021-03-19 10:44:35 -0300

    saving what i did before

---
+++ Foods/script.js
@@ -0,0 +1,16 @@
+const icons = document.querySelectorAll('.section-1-icons i')
+
+let i = 1
+
+setInterval(() => {
+  i++
+  const icon = document.querySelector('.section-1-icons .show')
+
+
+  icon.classList.remove('show')
+  if(i>icons.length) {
+    icons[0].classList.add('show');
+    i = 1
+  } 
+  icon.nextElementSibling.classList.add('show')
+},4000)
